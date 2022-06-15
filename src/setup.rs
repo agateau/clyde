65d7f6590106bc1f6783d7f8d553d421968088c3
Aurelien Gateau <mail@agateau.com>
2022-06-15 22:52:36 +0200

    Make setup create a bash/zsh activation script

--- src/setup.rs
+++ src/setup.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::include_str;
 use std::path::Path;
 use std::process::Command;
 
@@ -20,16 +21,34 @@ fn setup_store(store_dir: &Path) -> Result<()> {
     Ok(())
 }
 
+fn create_activate_script(app: &App) -> Result<()> {
+    let install_dir = app.install_dir.to_str().unwrap();
+    let content = format!(include_str!("activate.sh.tmpl"), install_dir = install_dir);
+
+    let scripts_dir = app.prefix.join("scripts");
+    let script_path = scripts_dir.join("activate.sh");
+    println!("Creating activation script");
+
+    fs::create_dir(&scripts_dir)?;
+    fs::write(&script_path, &content)?;
+
+    println!("\nAll set! To activate your Clyde installation, add this line to your shell startup script:\n\n\
+              . {script_path:?}");
+    Ok(())
+}
+
 pub fn setup(app: &App) -> Result<()> {
     if app.prefix.exists() {
         return Err(anyhow!("Clyde prefix directory ({:?}) already exists, not doing anything. Delete it if you want to start over.",
             app.prefix));
     }
-    println!("Setting up Clyde using prefix {:?}", app.prefix);
+    println!("Setting up Clyde in {:?}", app.prefix);
 
     fs::create_dir_all(&app.prefix)?;
 
     setup_store(&app.store_dir)?;
+
+    create_activate_script(&app)?;
 
     Ok(())
 }
