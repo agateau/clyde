9cc3bb049224977c22911ce19daed9dfc7c9169a
Aurelien Gateau <mail@agateau.com>
2022-06-20 08:20:34 +0200

    Make `show` show the installed version and list available versions in reverse order

--- src/show.rs
+++ src/show.rs
@@ -3,12 +3,19 @@ use anyhow::Result;
 use crate::app::App;
 
 pub fn show(app: &App, app_name: &str) -> Result<()> {
+    let db = app.get_database()?;
     let package = app.store.get_package(app_name)?;
+
     println!("Name: {}", package.name);
     println!("Description: {}", package.description);
 
+    if let Some(installed_version) = db.get_package_version(&package.name)? {
+        println!("Installed version: {}", installed_version);
+    }
+
+    println!();
     println!("Available versions:");
-    for version in package.releases.keys() {
+    for version in package.releases.keys().rev() {
         println!("- {}", version);
     }
     Ok(())
