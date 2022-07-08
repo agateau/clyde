e0767dea8abcb56504588561b33cdbc371c7cc51
Aurelien Gateau <mail@agateau.com>
2022-07-09 00:26:38 +0200

    clydetools: fix add-build arch-os detection

--- src/bin/clydetools/add_build.rs
+++ src/bin/clydetools/add_build.rs
@@ -1,6 +1,6 @@
-use std::collections::hash_map::Iter;
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
+use std::slice::Iter;
 
 use anyhow::{anyhow, Result};
 use semver::Version;
@@ -10,23 +10,36 @@ use clyde::checksum::compute_checksum;
 use clyde::file_cache::FileCache;
 use clyde::package::{Build, Package};
 
-type MatchingMap = HashMap<&'static str, Vec<&'static str>>;
+const ARCH_X86_64: &str = "x86_64";
+const ARCH_X86: &str = "x86";
+const ARCH_AARCH64: &str = "aarch64";
+
+const OS_LINUX: &str = "linux";
+const OS_MACOS: &str = "macos";
+const OS_WINDOWS: &str = "windows";
+
+type MatchingPair = (&'static str, &'static str);
 
 lazy_static! {
-    static ref ARCH_MAP: MatchingMap = {
-        let mut map = HashMap::new();
-        map.insert("x86_64", vec!["x86_64", "amd64"]);
-        map.insert("x86", vec!["x86", "386"]);
-        map.insert("aarch64", vec!["aarch64", "arm64"]);
-        map
-    };
-    static ref OS_MAP: MatchingMap = {
-        let mut map = HashMap::new();
-        map.insert("linux", vec!["linux"]);
-        map.insert("macos", vec!["macos", "apple", "darwin"]);
-        map.insert("windows", vec!["windows", "win32"]);
-        map
-    };
+    // Order matters: x86_64 must be looked for before x86
+    static ref ARCH_VEC: Vec<MatchingPair> = vec![
+        ("x86_64", ARCH_X86_64),
+        ("amd64", ARCH_X86_64),
+        ("x64", ARCH_X86_64),
+        ("x86", ARCH_X86),
+        ("386", ARCH_X86),
+        ("aarch64", ARCH_AARCH64),
+        ("arm64", ARCH_AARCH64),
+    ];
+    static ref OS_VEC: Vec<MatchingPair> = vec![
+        ("linux", OS_LINUX),
+        ("darwin", OS_MACOS),
+        ("apple", OS_MACOS),
+        ("macos", OS_MACOS),
+        ("windows", OS_WINDOWS),
+        ("win32", OS_WINDOWS),
+        ("win", OS_WINDOWS),
+    ];
 }
 
 fn compute_url_checksum(cache: &FileCache, url: &str) -> Result<String> {
@@ -34,24 +47,19 @@ fn compute_url_checksum(cache: &FileCache, url: &str) -> Result<String> {
     compute_checksum(&path)
 }
 
-// Must take an iterator as argument because each *_MAP is a unique type
-fn find_in_iter(
-    iter: Iter<'_, &'static str, Vec<&'static str>>,
-    name: &str,
-) -> Option<&'static str> {
-    for (key, tokens) in iter {
-        for token in tokens {
-            if name.contains(token) {
-                return Some(key);
-            }
+// Must take an iterator as argument because each *_VEC is a unique type
+fn find_in_iter(iter: Iter<'_, (&'static str, &'static str)>, name: &str) -> Option<&'static str> {
+    for (token, key) in iter {
+        if name.contains(token) {
+            return Some(key);
         }
     }
     None
 }
 
 fn extract_arch_os(name: &str) -> Option<ArchOs> {
-    let arch = find_in_iter(ARCH_MAP.iter(), name)?;
-    let os = find_in_iter(OS_MAP.iter(), name)?;
+    let arch = find_in_iter(ARCH_VEC.iter(), name)?;
+    let os = find_in_iter(OS_VEC.iter(), name)?;
     Some(ArchOs::new(arch, os))
 }
 
@@ -112,4 +120,31 @@ pub fn add_builds(
     new_package.to_file(path)?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn check_extract_arch_os(filename: &str, expected: Option<ArchOs>) {
+        let result = extract_arch_os(filename);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_extract_arch_os() {
+        check_extract_arch_os(
+            "foo-1.2-linux-arm64.tar.gz",
+            Some(ArchOs::new(ARCH_AARCH64, OS_LINUX)),
+        );
+        check_extract_arch_os(
+            "node-v16.16.0-win-x86.zip",
+            Some(ArchOs::new(ARCH_X86, OS_WINDOWS)),
+        );
+        check_extract_arch_os(
+            "node-v16.16.0-darwin-x64.tar.gz",
+            Some(ArchOs::new(ARCH_X86_64, OS_MACOS)),
+        );
+        check_extract_arch_os("bar-3.14.tar.gz", None);
+    }
 }
