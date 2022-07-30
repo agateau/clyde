7dd8466393159871f03c090a65b5190c9ac5d677
Aurelien Gateau <mail@agateau.com>
2022-07-30 11:35:03 +0200

    Use the Console crate to print colored indicators
    We already depends on this crate via the Indicatif crate, so might as well
    use it.

--- src/ui.rs
+++ src/ui.rs
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use console::{style, StyledObject};
+
 #[derive(Default)]
 pub struct Ui {
     level: u32,
@@ -26,19 +28,19 @@ impl Ui {
     }
 
     pub fn info(&self, msg: &str) {
-        self.print_with_indicator("I", msg);
+        self.print_with_indicator(style("[I]").blue(), msg);
     }
 
     pub fn warn(&self, msg: &str) {
-        self.print_with_indicator("W", msg);
+        self.print_with_indicator(style("[!]").yellow(), msg);
     }
 
     pub fn error(&self, msg: &str) {
-        self.print_with_indicator("E", msg);
+        self.print_with_indicator(style("[X]").red(), msg);
     }
 
-    fn print_with_indicator(&self, indicator: &str, msg: &str) {
+    fn print_with_indicator(&self, indicator: StyledObject<&str>, msg: &str) {
         self.print_indent();
-        eprintln!("[{indicator}] {}", msg);
+        eprintln!("{indicator} {}", msg);
     }
 }
