ce30ee8fe69dee5e5cb648de6a18997561fb6566
Aurelien Gateau <mail@agateau.com>
2024-03-30 15:56:17 +0100

    TarUnpacker: add test

--- src/unpacker/tar_unpacker.rs
+++ src/unpacker/tar_unpacker.rs
@@ -64,3 +64,76 @@ impl Unpacker for TarUnpacker {
         Ok(None)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use yare::parameterized;
+
+    use super::*;
+
+    use crate::test_file_utils::{get_fixture_path, list_tree, pathbufset_from_strings};
+
+    #[parameterized(
+        tar_gz = { "test_archive.tar.gz" },
+        tar_bz2 = { "test_archive.tar.bz2" },
+        tar_xz = { "test_archive.tar.xz" },
+    )]
+    fn test_unpack(filename: &str) {
+        // GIVEN a compressed tar archive
+        let path = get_fixture_path(filename);
+
+        let dir = assert_fs::TempDir::new().unwrap();
+
+        // AND a TarUnpacker on it
+        let unpacker = TarUnpacker::new(&path);
+
+        // WHEN unpack() is called
+        let ret = unpacker.unpack(&dir, 0).unwrap();
+        assert!(ret.is_none());
+
+        // THEN the archive content is copied in dir
+        let actual_files = list_tree(&dir).unwrap();
+        let expected_files = pathbufset_from_strings(&[
+            "hello/bin/hello",
+            "hello/bin/hello-symlink",
+            "hello/README.md",
+        ]);
+        assert_eq!(actual_files, expected_files);
+
+        #[cfg(unix)]
+        {
+            // AND hello/bin/hello is executable
+            use crate::test_file_utils::is_file_executable;
+            let exe_path = dir.join("hello/bin/hello").canonicalize().unwrap();
+            assert!(is_file_executable(&exe_path));
+
+            // AND hello/bin/hello-symlink points to hello/bin/hello
+            let symlink_path = dir.join("hello/bin/hello-symlink");
+            assert!(symlink_path.exists());
+            assert!(symlink_path.is_symlink());
+            let target_path = symlink_path.canonicalize().unwrap();
+            assert_eq!(target_path, exe_path);
+        }
+    }
+
+    #[test]
+    fn test_strip_components() {
+        // GIVEN a compressed tar archive
+        let path = get_fixture_path("test_archive.tar.gz");
+
+        let dir = assert_fs::TempDir::new().unwrap();
+
+        // AND a TarUnpacker on it
+        let unpacker = TarUnpacker::new(&path);
+
+        // WHEN unpack() is called with strip = 1
+        let ret = unpacker.unpack(&dir, 1).unwrap();
+        assert!(ret.is_none());
+
+        // THEN the archive content is copied in dir, but the top-level directory is not there
+        let actual_files = list_tree(&dir).unwrap();
+        let expected_files =
+            pathbufset_from_strings(&["bin/hello", "bin/hello-symlink", "README.md"]);
+        assert_eq!(actual_files, expected_files);
+    }
+}
